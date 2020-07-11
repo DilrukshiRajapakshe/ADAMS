71bf9bb29d622e05759be8f5f31ea2b592b453a1
D.D.S Rajapakshe <it16234062@my.sliit.lk>
2020-07-12 01:42:22 +0530

    zuul proxy implementation

---
+++ zuul_proxy/src/test/java/com/sliit/adams/zullrouting/ZullroutingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sliit.adams.zullrouting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZullroutingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
