50a6d3f70dfdfa6b53483d060799cdbe1a3a7d36
D.D.S Rajapakshe <it16234062@my.sliit.lk>
2020-08-19 10:37:52 +0530

    fileReader implementation

--- adams/lib/fileReader/service/FileReaderServiceImpl.dart
+++ adams/lib/fileReader/service/FileReaderServiceImpl.dart
@@ -2,13 +2,17 @@ import 'dart:async';
 import 'dart:core';
 import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:adams/fileReader/entity/JsonPath.dart';
 
-  Future<String>_loadFromAsset() async {
-    return await rootBundle.loadString("assets/images/config.json");
+  Future<String> _loadFromAsset() async {
+    return await rootBundle.loadString(JSON_PATH_URL);
   }
 
-  Future parseJson() async {
+  Future<String> parseJson(String service) async {
     String jsonString = await _loadFromAsset();
     final jsonResponse = jsonDecode(jsonString);
-    print(jsonResponse);
+    for(var value in jsonResponse ){
+      if (value['service'] ==  service) return (value['url']);
+    }
+    return 'Invalid service';
   }
