2078083ebe7c4fca2b8105f89785eba735d350dd
D.D.S Rajapakshe <it16234062@my.sliit.lk>
2020-11-17 21:33:40 +0530

    Developing an performance of Disease

--- Ontology/DOI-HM/app/Disease/Disease_Crud.py
+++ Ontology/DOI-HM/app/Disease/Disease_Crud.py
@@ -1,5 +1,8 @@
-def description(_Intent, _nameTypeList):
-    for i in range(len(_Intent)):
-        print("2")
+from app.CONSTANT import ONE_DISEASE
+from app.Main_Crud import getALL, getIndividual
+
+
+def description(_Intent, _action):
+    return getIndividual(getALL(ONE_DISEASE(_Intent[0])), _action)[0]
 
 
--- Ontology/DOI-HM/app/Disease/MainMethodDisease.py
+++ Ontology/DOI-HM/app/Disease/MainMethodDisease.py
@@ -1,6 +1,5 @@
 from app.Disease.Disease_Crud import description
 
 
-def disease_mainMethod(_Intent, _nameTypeList, sentence):
-    description(_Intent, _nameTypeList)
-    return "xxx"
+def disease_mainMethod(_Intent, _nameTypeList, sentence, _action):
+    return [description(_Intent, _action)]
