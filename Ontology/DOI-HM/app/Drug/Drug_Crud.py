e790120075a55e6d3c4a52a9b12a10a3bfea9b8d
D.D.S Rajapakshe <it16234062@my.sliit.lk>
2020-11-17 21:34:37 +0530

    Developing an performance of Drug

--- Ontology/DOI-HM/app/Drug/Drug_Crud.py
+++ Ontology/DOI-HM/app/Drug/Drug_Crud.py
@@ -1,3 +1,6 @@
-def description(_Intent, _nameTypeList):
-    for i in range(len(_Intent)):
-        print("3")
+from app.CONSTANT import ONE_DRUG
+from app.Main_Crud import getALL, getIndividual
+
+
+def description(_Intent, _action):
+    return getIndividual(getALL(ONE_DRUG(_Intent[0])), _action)[0]
