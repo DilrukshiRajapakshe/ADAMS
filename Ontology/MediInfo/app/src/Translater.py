97ff371d18eaa61fe9beb1fedf043d797dc995cf
D.D.S Rajapakshe <it16234062@my.sliit.lk>
2020-11-18 13:46:54 +0530

    .gitignore file edit

--- Ontology/MediInfo/app/src/Translater.py
+++
@@ -1,8 +0,0 @@
-# from googletrans import Translator
-from translate import Translator
-
-
-def translate(sentences):
-    translator = Translator()
-    result = translator.translate(sentences, dest='en')
-    return result.text
--- Ontology/MediInfo/app/src/disease.py
+++
@@ -1,9 +0,0 @@
-from app.src.mainService import runQuery, NameValue, keyWordIdentification
-
-
-def get_Decision_Of_disease(user_sentences):
-    list_drug = runQuery("service3")
-    name_list = NameValue(list_drug, user_sentences)
-    description_list = keyWordIdentification(name_list, "service3")
-
-    return description_list
