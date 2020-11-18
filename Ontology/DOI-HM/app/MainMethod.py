82e674d2d79c6b110e2857fdf71bc26ae0115337
D.D.S Rajapakshe <it16234062@my.sliit.lk>
2020-11-18 13:20:41 +0530

    performance development of Flask Rest API

--- Ontology/DOI-HM/app/MainMethod.py
+++ Ontology/DOI-HM/app/MainMethod.py
@@ -1,7 +1,9 @@
 from app.Disease.MainMethodDisease import disease_mainMethod
 from app.Drug.MainMethodDrug import drug_mainMethod
+from app.Drug_Prescribe.MainMethodDrugPrescribe import drug_prescribe_mainMethod
 from app.Error.Error import error_mainMethod
 from app.Question.MainMethodQuestion import question_mainMethod
+from app.Disease_Prescribe.MainMethodDiseasePrescribe import Disease_Prescribe_mainMethod
 from app.Symptoms.MainMethodSymptoms import symptoms_mainMethod
 
 
@@ -10,20 +12,27 @@ def mainMethod(sentence):
     _questionTypeList = _q_mainMethod["_questionTypeList"]
     _Intent = _q_mainMethod["_Intent"]
     _nameTypeList = _q_mainMethod["_nameTypeList"]
+    _action = _q_mainMethod["action"]
 
-    return switchCase(_questionTypeList, _Intent, _nameTypeList, sentence)
+    return switchCase(_questionTypeList, _Intent, _nameTypeList, _action, sentence)
 
 
-def switchCase(value, _Intent, _nameTypeList, sentence):
-    case = ""
+def switchCase(value, _Intent, _nameTypeList, _action, sentence):
+    case = []
 
-    if value in "Symptoms":
-        case = symptoms_mainMethod(_Intent, _nameTypeList, sentence),
-    elif value in "Drug":
-        case = drug_mainMethod(_Intent, _nameTypeList, sentence),
-    elif value in "Disease":
-        case = disease_mainMethod(_Intent, _nameTypeList, sentence),
+    if value == "Disease_Prescribe":
+        case = Disease_Prescribe_mainMethod(_Intent, _nameTypeList, sentence, _action)
+    elif value == "Drug_Prescribe":
+        case = drug_prescribe_mainMethod(_Intent, _nameTypeList, sentence, _action)
+    elif value == "Drug":
+        case = drug_mainMethod(_Intent, _nameTypeList, sentence, _action)
+    elif value == "Disease":
+        case = disease_mainMethod(_Intent, _nameTypeList, sentence, _action)
+    elif value == "Symptoms":
+        case = symptoms_mainMethod(_Intent, _nameTypeList, sentence, _action)
+    elif value == "Age":
+        case = disease_mainMethod(_Intent, _nameTypeList, sentence, _action)
     else:
-        case = error_mainMethod(_Intent, _nameTypeList, sentence)
+        case = error_mainMethod(_Intent, _nameTypeList, sentence, _action)
 
     return case
